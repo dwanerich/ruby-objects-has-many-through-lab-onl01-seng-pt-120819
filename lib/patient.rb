81f63f23697808f4d1d33cecc3bfd89c0ce6c14f
dwane richards <dwanerich@gmail.com>
2020-01-27 05:56:33 +0000

    Done.

--- lib/patient.rb
+++ lib/patient.rb
@@ -2,7 +2,7 @@ class Patient
 
   @@all = []
 
-  attr_accessor :name, :appointment
+  attr_accessor :name
   def initialize(name)
     @name = name
     @@all << self
@@ -18,7 +18,7 @@ class Patient
 
   def appointments
     Appointment.all.select do |appointment|
-      appointment.patient == self
+    appointment.patient == self
     end
   end
 
